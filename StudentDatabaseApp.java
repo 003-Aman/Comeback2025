6b8b264c042e80d58794334cf9ef2c1bce099a74
003-Aman <amanshrestha3003@gmail.com>
2025-01-15 02:03:34 -0500

    C8

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,11 @@
+import StudentDatabaseApp.Student;
+
+public class StudentDatabaseApp{
+    public static void main(String[] args) {
+        //ask how many new users we want to add
+        //create n number of new students
+        Student stu1 = new Student();
+        
+    }
+    
+}
---
+++ StudentDatabaseApp/Student.java
@@ -0,0 +1,61 @@
+package StudentDatabaseApp;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+    private static int id = 1000;
+    //constructor prompts user to enter student's name and year
+    public Student(){
+        System.out.println("Enter student first name: ");
+        Scanner in = new Scanner(System.in);
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear+ " " + studentID);
+        
+        
+        
+    }
+    //generate an ID
+
+    private void setStudentID(){
+        //grade level + ID
+        id++;
+        
+        this.studentID= gradeYear + "" + id;
+    }
+    //enroll in courses
+    public void enroll(){
+        //get inside a loop, user hits 0
+        do{
+            System.out.println("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if(!course.equals("Q")){
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else{
+                break;
+            }
+
+
+    }
+    //view balance
+    //pay tuition
+    //show status
+    
+}
