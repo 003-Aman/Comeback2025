91133fc821939db831d6c7d03c1450579d00c395
003-Aman <amanshrestha3003@gmail.com>
2025-02-13 01:29:43 -0500

    C22

--- Practice.cpp
+++ Practice.cpp
@@ -1,24 +1,48 @@
-#include <iostream>
+// #include <iostream>
 
-// Define needs_water() here:
-std::string needs_water(int days,bool is_succulent){
-  if(is_succulent ==false && days>3){
-    return "Time to water the plant.";
+// // Define needs_water() here:
+// std::string needs_water(int days,bool is_succulent){
+//   if(is_succulent ==false && days>3){
+//     return "Time to water the plant.";
+//   }
+//   else if(is_succulent == true && days ==12){
+//     return "Don't water the plant!";
+//   }
+//   else if(is_succulent == true && days>=13){
+//     return "Go ahead and give the plant a little water.";
+//   }
+//   else{
+//     return "Don't water the plant!";
+//   }
+// }
+
+
+// int main() {
+  
+//   std::cout << needs_water(10, false) << "\n";
+  
+// }
+
+bool is_palindrome(std::string text) {
+  std::string reversed_text = "";
+  
+  // Reverse the text
+  for (int i = text.size() - 1; i >= 0; i--) {
+    reversed_text += text[i];
   }
-  else if(is_succulent == true && days ==12){
-    return "Don't water the plant!";
-  }
-  else if(is_succulent == true && days>=13){
-    return "Go ahead and give the plant a little water.";
-  }
-  else{
-    return "Don't water the plant!";
-  }
+  
+  // Check if the reversed text is the same as the original
+  if(text == reversed_text){return true;}
+  else{return false;}
 }
+
+
 
 
 int main() {
   
-  std::cout << needs_water(10, false) << "\n";
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
   
 }
