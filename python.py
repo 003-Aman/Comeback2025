fa2f4117daa2a22247bb8e59822d2f5b9f0dd626
Aman Shrestha <amanshrestha3003@gmail.com>
2025-11-14 04:25:02 -0500

    Nov13

--- python.py
+++ python.py
@@ -7,3 +7,20 @@ print("Hello");print("Aman");print("Shrestha")
 x = 5               
 y="Hello, World"
 print(3+3)
+myVariableName = "Aman Shrestha" #camel case
+_my_variable_name = "Aman Shrestha" #snake case
+MyVariableName = "Aman Shrestha" #pascal case
+
+#MULTIPLE VARIABLES
+a, b, c = "Orange", "Banana", "Cherry"
+
+#UNPACK A COLLECTION
+fruits = ["Apple", "Banana", "Cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x ="Python"
+y=" is "
+z="awesome"
+print(x, y, z)
