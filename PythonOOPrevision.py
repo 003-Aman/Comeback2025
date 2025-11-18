6baea4b808fabeda55a20f684342568f775f3ff0
Aman Shrestha <amanshrestha3003@gmail.com>
2025-11-17 21:07:03 -0500

    NOV 17

--- PythonOOPrevision.py
+++ PythonOOPrevision.py
@@ -9,12 +9,18 @@ print(s1.section)# were asking for the attribute
 #THE __init__() method , which is the constructor. in my own words, this is a function that tells the objects what attribute it has or how to behave when it is created
 class Cars:
     def __init__(self,make,model,year):
+        self.country = "USA"# this can be set as default attribute for every object created. dont have to take it in as a parameter
         self.make = make
         self.model = model
         self.year = year
+
+    def drive(self):
+        print("VROOM VROOM "+self.model+" is driving!")
 
 car1= Cars("Hyundai","Elantra",2020)
 print(car1.make)
 print(car1.model)
 print(car1.year)
+print(car1.country)
+car1.drive()
         
